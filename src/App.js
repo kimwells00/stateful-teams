70a207203cd8ec3ec7f2e295915ccdbfb85bc5b4
Joseph <frasier.joseph@gmail.com>
2023-02-08 08:18:33 -0600

    for cohort 11-22-22

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import "./App.css";
 import MainContainer from "./components/MainContainer";
 import { useState } from "react";
-import ciara from "./assets/ciara.png";
-import jason from "./assets/jason.png";
-import rayleigh from "./assets/rayleigh.png";
-import santos from "./assets/santos.png";
-
+import adam from "./assets/adam.png";
+import corey from "./assets/corey.png";
+import jaye from "./assets/jaye.png";
+import jess from "./assets/jess.png";
+import mauro from "./assets/mauro.png";
+import rokhaya from "./assets/rokhaya.png";
+import vinny from "./assets/vinny.png";
+import meg from "./assets/meg.png";
+import peter from "./assets/peter.png";
 function App() {
   const studentData = [
-    { name: "Ciara", img: ciara },
-    { name: "Rayleigh", img: rayleigh },
-    { name: "Santos", img: santos },
-    { name: "Jason", img: jason },
+    { name: "adam", img: adam },
+    { name: "corey", img: corey },
+    { name: "jaye", img: jaye },
+    { name: "jess", img: jess },
+    { name: "mauro", img: mauro },
+    { name: "rokhaya", img: rokhaya },
+    { name: "vinny", img: vinny },
+    { name: "meg", img: meg },
+    { name: "peter", img: peter },
   ];
   const [students, setStudents] = useState(studentData);
   return (
